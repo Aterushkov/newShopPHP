87304d02fe0c3b68816194adfcfcf6154b923956
Aterushkov <Sanek.s88@gmail.com>
2020-05-07 20:03:18 +0500

    07/05 вечер

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@ require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
 
 new \tavshop\App();
-
-debug(\tavshop\App::$app->getProperties());
+// debug(\tavshop\App::$app->getProperties());
+// throw new Exception('Страница не найдена', 404);
