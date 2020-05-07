a6ddd535a72de7a6087ef284da7f09b283733203
Aterushkov <Sanek.s88@gmail.com>
2020-05-07 10:46:16 +0500

    07/05

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?
+
+return [
+  'admin_email' => 'admin@email.ru',
+  'shop_name' => 'Магазин TAV',
+  'pagination' => '3',
+  'smtp_login' => '',
+  'smtp_password' => '',
+];
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?
+require_once dirname(__DIR__) . '/config/init.php';
+require_once LIBS . '/functions.php';
+
+new \tavshop\App();
+debug(\tavshop\App::$app->getProperties());
