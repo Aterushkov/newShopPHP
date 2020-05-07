982de7c280de99c6ba1b4992c97b56ece0819ab9
Aterushkov <Sanek.s88@gmail.com>
2020-05-07 22:08:54 +0500

    07.05 night

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \tavshop\App();
-// debug(\tavshop\App::$app->getProperties());
+
+//  debug(\tavshop\App::$app->getProperties());
+debug(\tavshop\Router::getRoutes());
 // throw new Exception('Страница не найдена', 404);
