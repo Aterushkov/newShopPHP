a6659a71dec5c502eb5973aba0f56fe237e1aa62
Aterushkov <Sanek.s88@gmail.com>
2020-05-09 17:27:40 +0500

    Cache ready

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,6 +1,7 @@
 <?
 namespace app\controllers;
 
+use tavshop\Cache;
 
 class MainController extends AppController{
 
@@ -8,6 +9,17 @@ class MainController extends AppController{
   public function indexAction(){   
     $posts = \R::findAll('test');
     $this->setMeta('Главная страница', 'Описание главное страницы', 'Ключи главной страницы');
+    
+    $names =['Fyr','Ryf','myd'];
+    $cache = Cache::instance();
+    // $cache->delite('test');
+    $data = $cache->get('test');
+    if(!$data){
+      $cache->set('test', $names);
+    }
+    
+    debug($data);
+
     $this->set(compact(['posts']));
   }
   
