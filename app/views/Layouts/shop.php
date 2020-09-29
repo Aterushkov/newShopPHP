b8c390d71701c1c9eda60a5242649d3ed7848b7a
Dom <Sanek.s88@gmail.com>
2020-09-29 19:41:23 +0300

    29.09 Search start

--- app/views/Layouts/shop.php
+++ app/views/Layouts/shop.php
@@ -47,12 +47,6 @@
                                 <?endif;?>
                             </div>
                         </a>
-<!--						<a href="/checkout.html">-->
-<!--							 <div class="total">-->
-<!--								<span class="simpleCart_total"></span></div>-->
-<!--								<img src="/images/cart-1.png" alt="" />-->
-<!--						</a>-->
-<!--						<p><a href="/javascript:;" class="simpleCart_empty">Empty Cart</a></p>-->
 						<div class="clearfix"> </div>
 					</div>
 				</div>
@@ -86,8 +80,12 @@
 			</div>
 			<div class="col-md-3 header-right"> 
 				<div class="search-bar">
-					<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
-					<input type="submit" value="">
+                    <form action="search" method="get" autocomplete="off">
+                        <input type="text" class="typeahead" id="typeahead" name="s">
+                        <input type="submit" value="">
+                    </form>
+<!--					<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">-->
+<!--					<input type="submit" value="">-->
 				</div>
 			</div>
 			<div class="clearfix"> </div>
@@ -191,6 +189,7 @@
     </script>
 	<script src="/js/jquery-1.11.0.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/typeahead.bundle.js"></script>
 	<script src="js/imagezoom.js"></script>
 	<script defer src="js/jquery.flexslider.js"></script>
 	<script src="/js/jquery.easydropdown.js"></script>	
