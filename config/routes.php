ad614f5f7731f4d81da65c3afed0d3ee207ee5e6
Dom <Sanek.s88@gmail.com>
2020-10-05 22:59:05 +0300

    pagination end

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@
 use tavshop\Router;
 
 Router::add('^product/(?P<alias>[a-z0-9-]+)/?$',['controller' =>'Product', 'action' => 'view']);
+Router::add('^category/(?P<alias>[a-z0-9-]+)/?$',['controller' =>'Category', 'action' => 'view']);
 
 // default routes admin
 Router::add('^admintav$',['controller' =>'Main', 'action' => 'index', 'prefix' =>'admintav']);
